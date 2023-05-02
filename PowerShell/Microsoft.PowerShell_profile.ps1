7561f0acf4d2386ed075b68130a641988c8f3f37
Martin Dennhardt (SHS DI CT R&D REL DP INST) <martin.dennhardt@siemens-healthineers.com>
2023-05-02 09:05:59 +0200

    Fix pwsh settings

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,12 @@
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/M365Princess.omp.json' | Invoke-Expression
 Set-PSReadlineOption -PredictionSource History
 Set-PSReadLineKeyHandler Tab -Function MenuComplete
+Set-PSReadLineOption -ShowToolTips:$false
 $Colors = @{
   "Command"            = "$([char]0x1b)[93m"
   "Comment"            = "$([char]0x1b)[32m"
-  "ContinuationPrompt" = "$([char]0x1b)[90m"
+  "ContinuationPrompt" = "$([char]0x1b)[37m"
+  "InlinePrediction"   = "$([char]0x1b)[37m"
   "Default"            = "$([char]0x1b)[30m"
   "Emphasis"           = "$([char]0x1b)[96m"
   "Error"              = "$([char]0x1b)[91m"
@@ -13,10 +15,12 @@ $Colors = @{
   "Number"             = "$([char]0x1b)[90m"
   "Operator"           = "$([char]0x1b)[90;1m"
   "Parameter"          = "$([char]0x1b)[90;1m"
-  "Selection"          = "$([char]0x1b)[37;40m"
+  "Selection"          = "$([char]0x1b)[30;7m"
   "String"             = "$([char]0x1b)[36m"
   "Type"               = "$([char]0x1b)[30m"
   "Variable"           = "$([char]0x1b)[92m"
+  "ListPrediction"     = "$([char]0x1b)[33m"
+  "ListPredictionSelected" = "$([char]0x1b)[33;7m"
 }
 Set-PSReadlineOption -Colors $Colors
 Import-Module Terminal-Icons
